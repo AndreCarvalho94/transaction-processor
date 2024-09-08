7b6ab63059e679b10cd5a3687294220ee10253ce
Andre Carvalho <t.andre.pinheiro@estapar.com.br>
2024-09-08 19:58:46 -0300

    read balance feature ok

--- src/main/java/br/com/acdev/transaction_processor/service/AccountService.java
+++ src/main/java/br/com/acdev/transaction_processor/service/AccountService.java
@@ -3,10 +3,15 @@ package br.com.acdev.transaction_processor.service;
 import br.com.acdev.transaction_processor.exceptions.InvalidLimitException;
 import br.com.acdev.transaction_processor.exceptions.NotFoundException;
 import br.com.acdev.transaction_processor.model.Account;
+import br.com.acdev.transaction_processor.model.OperationType;
+import br.com.acdev.transaction_processor.model.Transaction;
 import br.com.acdev.transaction_processor.repository.AccountRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 import java.math.BigDecimal;
 
@@ -30,5 +35,16 @@ public class AccountService {
 
     public Account read(Long id){
         return repository.findById(id).orElseThrow(()->new NotFoundException(ACCOUNT_NOT_FOUND));
+    }
+
+    public BigDecimal readBalance(Long id) {
+        Account account = repository
+                .findById(id)
+                .orElseThrow(()->new NotFoundException("Account not found"));
+        return account
+                .getTransactions().stream()
+                .map(Transaction::getAmount)
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
     }
 }
